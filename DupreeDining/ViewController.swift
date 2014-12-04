a3a3d15ed4484987d63b2b1fa63b1210e98f97bd
redriverit <dsimpson@redriveritc.com>
2014-12-04 16:10:51 -0600

    Blank Xcode in Swift with Parse

---
+++ DupreeDining/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DupreeDining
+//
+//  Created by Daniel Simpson on 12/4/14.
+//  Copyright (c) 2014 Red River IT Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Test.m
@@ -0,0 +1,9 @@
+//
+//  Test.m
+//  DupreeDining
+//
+//  Created by Daniel Simpson on 12/4/14.
+//  Copyright (c) 2014 Red River IT Consulting. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
